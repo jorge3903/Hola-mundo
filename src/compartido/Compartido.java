3074607b2ac9fee26663339b4dc05465d4de9278
hcoria <36715277+hcoria@users.noreply.github.com>
2018-03-03 22:48:50 -0600

    prueba
    solo es una prueba

--- src/compartido/Compartido.java
+++ src/compartido/Compartido.java
@@ -1,4 +1,4 @@
-/*comentario de prueba
+/*comentario de prueba ok
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
