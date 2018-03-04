ed98ee643aad138ca2960b39e3ab583a418360c8
HC <HC@192.168.1.81>
2018-03-03 23:17:07 -0600

    nuevo desde netbeans prueba

--- src/compartido/Compartido.java
+++ src/compartido/Compartido.java
@@ -1,4 +1,4 @@
-/*comentario de prueba ok
+/*comentario de prueba ok cambio hecho desde netbeans
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
