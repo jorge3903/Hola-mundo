06b5670f3088b067ae0ec5b61f21890d884f45f4
lalo <lalo@lalo-HP-1000-Notebook-PC>
2018-03-03 21:45:37 -0600

    version 0.0.1

---
+++ src/compartido/Compartido.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compartido;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lalo
+ */
+public class Compartido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"agrega lo que quieras");
+    }
+    
+}
